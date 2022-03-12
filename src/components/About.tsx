2bcecd58dbab1fb631e7f594c0d80825377bbfb8
ashpool <ashpool@metaverse.io>
2022-03-12 08:05:37 -0700

    updates instructions on Home.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,8 @@ function About() {
                         <li>[white paper]: <a className="App-link" href="https://github.com/microtitle-io/crosslink-protocol/blob/main/docs/crosslink-protocol-v0.1b.pdf" target="_blank" rel="noopener noreferrer">crosslink-protocol-v0.1b.pdf</a></li>
                         <li>[medium]: <a href="https://medium.com/@microtitle">https://medium.com/@microtitle</a></li>
                         <li>[twitter]: <a href="https://twitter.com/microtitle_io">https://twitter.com/microtitle_io</a></li>
-                        <li>[github]: <a href="https://github.com/microtitle-io/crosslink-protocol">https://github.com/microtitle-io/crosslink-protocol</a></li>
+                        <li>[github - client]: <a href="https://github.com/microtitle-io/crosslink-protocol">https://github.com/microtitle-io/crosslink-protocol</a></li>
+                        <li>[github - anchor]: <a href="https://github.com/microtitle-io/xlp_smart_contract">https://github.com/microtitle-io/xlp_smart_contract</a></li>
                     </ul>
                 </div>
             </div>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,6 +8,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import {Routes, Route, Link} from 'react-router-dom';
+
 
 function Home() {
     return (
@@ -20,24 +22,21 @@ function Home() {
                         <Table sx={{ minWidth: 650 }}  aria-label="how-to table">
                           <TableBody>
                               <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">Step 1</TableCell>
-                                <TableCell align="right">Create a "bonding" keypair</TableCell>
+                                <TableCell component="th" scope="row">Step 1: <a href="/sign">Sign</a></TableCell>
+                                <TableCell align="right">Create a bonding keypair, create a signature (QR code), and attach it to your item.</TableCell>
+                                {/* <TableCell align="right">Create a bonding keypair, create a signature (2D QR code), and attach to your item.<br/><img src={qrcode} className="qrcode" alt="Example QR code signature." /></TableCell> */}
                               </TableRow>
                               <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">Step 2</TableCell>
-                                <TableCell align="right">Generate signature (2D QR code)<br/><img src={qrcode} className="qrcode" alt="Example QR code signature." /></TableCell>
+                                <TableCell component="th" scope="row">Step 2: <a href="/mint">Mint</a></TableCell>
+                                <TableCell align="right"> Mint the NFT "microtitle."</TableCell>
                               </TableRow>
                               <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">Step 3</TableCell>
-                                <TableCell align="right">Permanently attach signature to item</TableCell>
+                                <TableCell component="th" scope="row">Step 3: <a href="/register">Register</a></TableCell>
+                                <TableCell align="right">Use our smart contract to "link" the NFT mint ID to the item's bonding key.</TableCell>
                               </TableRow>
                               <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">Step 4</TableCell>
-                                <TableCell align="right">Mint the NFT "microtitle"</TableCell>
-                              </TableRow>
-                              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">Step 5</TableCell>
-                                <TableCell align="right">Verify signature against the microtitle at any time!</TableCell>
+                                <TableCell component="th" scope="row">Step 4: <a href="/verify">Verify</a></TableCell>
+                                <TableCell align="right"> Verify signature against the microtitle at any time!</TableCell>
                               </TableRow>
                           </TableBody>
                         </Table>
